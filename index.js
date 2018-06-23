e757941d2383a10fe09f4fee01a42a74d2853f5d
wuzhx <15010057138@163.com>
2018-06-23 17:45:07 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const { execFile } = require('child_process');
-const child = execFile('git', ['status'], (error, stdout, stderr) => {
+const { execFile,execSync } = require('child_process');
+// execFile('git', ['status'], (error, stdout, stderr) => {
+//     if (error) {
+//         throw error;
+//     }
+//     console.log(stdout);
+// });
+
+
+execSync('git add .', (error, stdout, stderr) => {
     if (error) {
         throw error;
     }
     console.log(stdout);
 });
 
-child.on('exit',(error)=>{
-    console.log(error);
-})
+
+execSync('git commit -m "update"', (error, stdout, stderr) => {
+    if (error) {
+        throw error;
+    }
+    console.log(stdout);
+});
